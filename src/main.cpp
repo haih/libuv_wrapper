3c4cda0b397fe105c6d6a0d607106585c4cbd9a1
Hai Hu <huhai2005@126.com>
2014-03-26 09:46:59 +0800

    add main.cpp  and  empty header files

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+*main.cpp 
+*/
+
+#include "uv_wrapper.h"
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
---
+++ src/uv_wrapper.h
@@ -0,0 +1,13 @@
+/*
+*uv_wrapper.h
+*/
+
+#ifndef UV_WRAPPER_H
+#define UV_WRAPPER_H
+
+#include <iostream>
+using namespace std;
+
+#include "uv.h"
+
+#endif
